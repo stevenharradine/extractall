5477c0df4fde1aa515d7fd45ea06eb1f45e32219
Steven Harradine <stevenharradine@gmail.com>
2017-05-20 00:07:59 -0400

    mvp

---
+++ README.md
@@ -0,0 +1,22 @@
+# ExtractAll
+Extracts various file types with one command
+
+## Supported file formats
+ * zip
+ * tar.gz
+ * gz
+
+## Requirements
+ * unzip
+ * tar
+ * gunzip
+
+## usage
+ 1. install
+```
+sudo bash extractall.sh install
+```
+ 2. use
+ ```
+ extractall test.zip
+ ```
---
+++ extractall.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+installPath=/usr/local/bin
+
+if [[ "$1" == "install" ]]; then
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root"
+	else
+		echo -n "Installing . "
+		cp extractall.sh $installPath/extractall
+		chown root:root $installPath/extractall
+		chmod 755 $installPath/extractall
+		echo "Done"
+	fi
+else
+	filename=$1
+
+	if [[ "$filename" == *.zip ]]; then
+		echo "Zip detected"
+		unzip $filename
+	elif [[ "$filename" == *.tar.gz ]]; then
+		echo "TAR Gunzip detected"
+		tar -zxvf $filename
+	elif [[ "$filename" == *.gz ]]; then
+		echo "Gunzip detected"
+		gunzip $filename
+	else
+		echo "Unknown format"
+	fi
+fi
